ce6abe3261eb01c1604d746aec256a80536bfb57
Rob Eisenberg <rob@bluespire.com>
2015-01-06 13:45:34 -0500

    feat(build): update compile, switch to register modules, switch to core-js

--- config.js
+++ config.js
@@ -1,20 +1,28 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "aurelia-router/*": "dist/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
 System.config({
   "map": {
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
-    "aurelia-history": "github:aurelia/history@0.1.1",
-    "aurelia-path": "github:aurelia/path@0.3.0",
-    "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.1.1",
-    "es6-shim": "github:paulmillr/es6-shim@0.21.1",
-    "github:aurelia/dependency-injection@0.1.1": {
-      "aurelia-metadata": "github:aurelia/metadata@0.1.1",
-      "es6-shim": "github:paulmillr/es6-shim@0.21.1"
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.2.1",
+    "aurelia-history": "github:aurelia/history@0.2.0",
+    "aurelia-path": "github:aurelia/path@0.4.0",
+    "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.2.0",
+    "core-js": "npm:core-js@0.4.1",
+    "github:aurelia/dependency-injection@0.2.1": {
+      "aurelia-metadata": "github:aurelia/metadata@0.2.3",
+      "core-js": "npm:core-js@0.4.1"
+    },
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "npm:core-js@0.4.1": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
     }
   }
 });
