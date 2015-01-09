8ae26b56ea3bb76971a775f738468c840cc3f979
Klaus Andrey <deepmindster@gmail.com>
2015-01-10 01:32:54 +0300

    test(navigation-commands): add tests
    Add tests for class Redirect and for function isNavigationCommand.

---
+++ test/navigation-commands.spec.js
@@ -0,0 +1,37 @@
+import {Redirect, isNavigationCommand} from '../src/navigation-commands';
+
+describe('isNavigaionCommand', () => {
+  it('should return true for object which has a navigate method', () => {
+    var nc = {
+      navigate: function() {}
+    }
+
+    expect(isNavigationCommand(nc)).toBe(true)
+  })
+
+  it('should return false for everything that does not have a navigate method', () => {
+    expect(isNavigationCommand(true)).toBe(false)
+    expect(isNavigationCommand(1)).toBe(false)
+    expect(isNavigationCommand({})).toBe(false)
+  })
+})
+
+describe('Redirect', () => {
+    it('should accept url in constructor and pass this url to passed router\'s navigate method as first parameter', () => {
+	var testurl = 'http://aurelia.io/'
+	var redirect = new Redirect(testurl)
+
+	var mockrouter = {
+	    url: '',
+	    navigate: function(url) {
+		this.url = url
+	    }
+	}
+
+	expect(mockrouter.url).toBe('')
+
+	redirect.navigate(mockrouter)
+
+	expect(mockrouter.url).toBe(testurl)
+    })
+})
