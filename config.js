3e8dac3d3e0f80939ee25ae935f0db4f054f448a
Rob Eisenberg <rob@bluespire.com>
2015-01-12 09:22:24 -0500

    fix(package): update Aurelia dependencies

--- config.js
+++ config.js
@@ -9,19 +9,19 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.2.1",
-    "aurelia-history": "github:aurelia/history@0.2.0",
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.3.1",
+    "aurelia-history": "github:aurelia/history@0.2.1",
     "aurelia-path": "github:aurelia/path@0.4.0",
-    "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.2.0",
-    "core-js": "npm:core-js@0.4.1",
-    "github:aurelia/dependency-injection@0.2.1": {
-      "aurelia-metadata": "github:aurelia/metadata@0.2.3",
-      "core-js": "npm:core-js@0.4.1"
+    "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.2.1",
+    "core-js": "npm:core-js@0.4.4",
+    "github:aurelia/dependency-injection@0.3.1": {
+      "aurelia-metadata": "github:aurelia/metadata@0.2.4",
+      "core-js": "npm:core-js@0.4.4"
     },
     "github:jspm/nodelibs-process@0.1.0": {
       "process": "npm:process@0.10.0"
     },
-    "npm:core-js@0.4.1": {
+    "npm:core-js@0.4.4": {
       "process": "github:jspm/nodelibs-process@0.1.0"
     }
   }
