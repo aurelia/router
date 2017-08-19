40d0882c1b33604a037b40e453e90307d7041421
Aluan Haddad <aluanh@gmail.com>
2017-08-19 16:29:37 -0400

    improve consistency

--- src/interfaces.js
+++ src/interfaces.js
@@ -176,11 +176,11 @@ interface ConfiguresRouter {
 */
 interface ActivationStrategy {
   /**
-  * Reuse the existing view model without invoking lifecycle hooks.
+  * Reuse the existing view model, without invoking Router lifecycle hooks.
   */
   noChange: 'no-change';
   /**
-  * Reuse the existing view model, invoke Router lifecycle hooks.
+  * Reuse the existing view model, invoking Router lifecycle hooks.
   */
   invokeLifecycle: 'invoke-lifecycle';
   /**
