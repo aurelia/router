f220cfef31173e08a6821a04a3b787bb3a6d519e
Raphael Luba <raphael@leanbyte.com>
2018-06-02 22:23:53 +0200

    Add a task `gulp dev [--target <module>]`
    This supports cross-module development by allowing you to test changes of one module in the context of another module.

--- build/tasks/dev.js
+++ build/tasks/dev.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
-var tools = require('aurelia-tools');
+const fs = require('fs');
+const path = require('path');
+
+const gulp = require('gulp');
+const tools = require('aurelia-tools');
+const glob = require("glob")
+const paths = require('../paths');
+const args = require('yargs')
+    .options('target', {
+        alias: 't',
+        description: 'target module dir to copy build results into (eg. "--target ../other-module" to copy build results into "../other-module/node_modules/this-module/dist/…" whenever they change)',
+    })
+    .argv;
 
 gulp.task('update-own-deps', function(){
   tools.updateOwnDependenciesFromLocalRepositories();
@@ -7,4 +18,41 @@ gulp.task('update-own-deps', function(){
 
 gulp.task('build-dev-env', function () {
   tools.buildDevEnv();
+});
+
+gulp.task('dev', ['build'], () => {
+  gulp.watch(paths.files, ['build']);
+
+  if (args.target) {
+    const projectName = require('../../package.json').name;
+
+    const targetNPMPath = path.join(args.target, 'node_modules', projectName);
+    fs.stat(targetNPMPath, (err, stats) => {
+      if (!err && stats.isDirectory()) {
+        const targetNPMDistPath = path.join(targetNPMPath, paths.output);
+        watchAndCopy(paths.output, targetNPMDistPath);
+      }
+    });
+
+    const targetJSPMGlob = path.join(args.target, 'jspm_packages', 'npm', projectName + '@*/');
+    glob(targetJSPMGlob, (err, files) => {
+      if (!err && files.length === 1) {
+        const targetJSPMPath = files[0];
+        const amdPath = path.join(paths.output, 'amd');
+        watchAndCopy(amdPath, targetJSPMPath);
+      }
+    });
+
+    function watchAndCopy(basePath, targetPath) {
+        gulp.watch(path.join(basePath, '**', '*'), (event) => {
+          if (event.type === 'deleted') {
+            return;
+          }
+
+          const relativePath = path.relative(basePath, event.path);
+          const destPath = path.join(targetPath, relativePath);
+          fs.createReadStream(event.path).pipe(fs.createWriteStream(destPath));
+        });
+    }
+  }
 });
