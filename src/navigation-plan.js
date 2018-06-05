aurelia/router

commit 487c33d50fa938c9e725180877f6510ecae89db5

fix(redirect): do not use Promise.reject on redirect
Closes #480

src/navigation-plan.js
@@ -13,6 +13,9 @@ export class BuildNavigationPlanStep {
   run(navigationInstruction: NavigationInstruction, next: Function) {
     return _buildNavigationPlan(navigationInstruction)
       .then(plan => {
+        if (plan instanceof Redirect) {
+          return next.cancel(plan);
+        }
         navigationInstruction.plan = plan;
         return next();
       }).catch(next.cancel);
@@ -33,7 +36,7 @@ export function _buildNavigationPlan(instruction: NavigationInstruction, forceLi
         redirectLocation += '?' + instruction.queryString;
       }
 
-      return Promise.reject(new Redirect(redirectLocation));
+      return Promise.resolve(new Redirect(redirectLocation));
     });
   }
 

commit 3db8a8cc68f2c2ba6ba2ae16c322491d389dc984

Fix error introduced in 487c33d50fa938c9e725180877f6510ecae89db5 (when trying to fix #480)
This causes #480 to re-appear until a better handling for cancellations exists

src/navigation-plan.js
@@ -85,6 +85,9 @@ export function _buildNavigationPlan(instruction: NavigationInstruction, forceLi
               childInstruction,
               viewPortPlan.strategy === activationStrategy.invokeLifecycle)
               .then(childPlan => {
+                if (childPlan instanceof Redirect) {
+                  return Promise.reject(childPlan);
+                }
                 childInstruction.plan = childPlan;
               });
           });

src/route-loading.js
@@ -81,6 +81,9 @@ function loadRoute(routeLoader: RouteLoader, navigationInstruction: NavigationIn
 
           return _buildNavigationPlan(childInstruction)
             .then((childPlan) => {
+              if (childPlan instanceof Redirect) {
+                return Promise.reject(childPlan);
+              }
               childInstruction.plan = childPlan;
               viewPortInstruction.childNavigationInstruction = childInstruction;
 